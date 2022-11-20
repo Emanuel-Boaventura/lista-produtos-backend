c4251ac0e135f19fbe17de373282e0a033f5c4ce
Emanuel-Boaventura <emanuelbmatos321@gmail.com>
2022-11-20 16:44:36 -0400

    add cors policy

--- src/app.js
+++ src/app.js
@@ -1,20 +1,24 @@
 const { products } = require('./database');
 const express = require('express');
 const productsRouter = require('./Products/Routes');
+const cors = require('cors');
 
 const app = express();
+
+app.use(
+  cors({
+    origin: '*',
+  })
+);
+
 app.use(express.json());
 
 app.use('/products', productsRouter);
-
-app.listen(3333, () => {
-  console.log('App running in port 3333');
-});
 
 app.get('/', function (req, res) {
   res.status(200).json('API Fullstack Job Test - DomPixel running');
 });
 
-// PUT "/products/:productId" Será responsável por receber atualizações de um produto especifico enviados pelo frontend
-
-// DELETE "/products/:productId" Remover o produto da base de dados
+app.listen(3333, () => {
+  console.log('App running in port 3333');
+});
